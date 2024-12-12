46d4b2f0e46db584caa1d48cfe3e398559a67fc8
Sundar Ghodadara <142219396+SundarGhodadara@users.noreply.github.com>
2024-12-12 17:01:40 +0530

    Link Update

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const logoutRouter = require('./routes/logout-router');
 const contactRouter = require('./routes/contact-router');
 
 //middleware
-app.use(cors({origin:"http://localhost:5173", credentials:true, methods:['GET','POST','PUT','DELETE'] })); //CORS allocation
+app.use(cors({origin:"https://tanked-up.onrender.com", credentials:true, methods:['GET','POST','PUT','DELETE'] })); //CORS allocation
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieparser());
